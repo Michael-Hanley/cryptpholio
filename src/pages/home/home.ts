ba2985f31bff52ff53663f0208dec338bf43bd53
Michael Hanley <pasaquali@gmail.com>
2017-08-12 16:54:12 -0700

    converted to searchbar and displaying result

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,16 +11,18 @@ export class HomePage {
   coinPrices: Array<any> = [];
   errorMessage: string;
   searchedCoin: string = '';
+  allCoins:Array<any> = [];
+  myCoins:Array<any> = [];
   constructor(public navCtrl: NavController, private dataService:DataService) {
-   this.getCoinPrice();
+    this.getCoins();
   }
     getCoins(){
       this.dataService.getCoins()
         .subscribe((coinInfo) => {
-          //this.getCoinPrice(coinInfo);
-          this.coins = coinInfo;
+          this.allCoins = coinInfo;
         });
       }
+      /*
     getCoinPrice(){
       let data: Array<any>;
       for (var coin of Object.keys(this.searchedCoin)) {
@@ -28,8 +30,30 @@ export class HomePage {
           .subscribe((coinsPrice) => {
               data = coinsPrice;
               this.coins.push(data);
-              console.log(data);
+              console.log(this.coins);
         })
       }
     }
+    */
+    getCoinPrice(){
+      this.dataService.getCoinPrice(
+        this.searchedCoin = this.searchedCoin.toUpperCase())
+        .subscribe((coinsPrice) => {
+            this.coins = [];
+            this.coins.push(coinsPrice);
+      })
+    }
+
+    addCoin(){
+      let haveCoin: boolean = false;
+      this.myCoins.forEach(element => {
+        if (element == this.searchedCoin){
+          haveCoin = true;
+        }
+      });
+      if(haveCoin == false){
+        this.myCoins.push(this.searchedCoin);
+      }
+    }
+
 }
